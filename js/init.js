ec9ea5f5e6ff9632c38dfde78ced25f736775037
ErickElCapo86 <87632516+ErickElCapo86@users.noreply.github.com>
2021-07-19 01:28:17 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyBJtj1ZRkKUscJJjcgHHsZBUWolBQhNtKs",
+    authDomain: "autendwp.firebaseapp.com",
+    projectId: "autendwp",
+    storageBucket: "autendwp.appspot.com",
+    messagingSenderId: "448427692326",
+    appId: "1:448427692326:web:eac0036e3bff82220f71e7",
+    measurementId: "G-67RQX3D7KH"
+});
